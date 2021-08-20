66364ffe8a23631b96ed4371ef721a64441a9851
bthalinda <bthalinda@gmail.com>
2021-08-20 09:55:16 +0530

    swagger remaing bug

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var swaggerJSdoc = require('swagger-jsdoc')
+var swaggerExpress = require('swagger-ui-express');
 var orders = require('./routes/orders');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const swaggerOptions = {
+   swaggerDefinition:{
+      info:{
+         title:"Order Manager",
+         version:"1.0.0",
+         servers:["http://loclhost:8081"]
+      }
+   },
+   apis: ['./routes*.js'],
+}
+
+const swaggerdoc = swaggerJSdoc(swaggerOptions)
+console.log(swaggerdoc)
+app.use('/api-docs',swaggerExpress.serve,swaggerExpress.setup(swaggerdoc))
+
+/**
+ * @swagger
+ * /orders:
+ *    get:
+ *     description: Use to request
+ *     responses:
+ *       '200':
+ *          description:A successfully respine
+ *
+*/
 app.use('/orders',orders);
-
-
-
 
 module.exports = server = app.listen(8081, function () {
    var host = server.address().address
